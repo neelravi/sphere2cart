65e1b01565dc8e5d71dceca156a0af97a2bf5fca
Ravindra Shinde <neelravi@gmail.com>
2022-03-23 15:05:14 +0100

    new ordering in bfinfo obtained

--- old2new.py
+++ old2new.py
@@ -6,17 +6,16 @@ np.set_printoptions(threshold=np.inf)
 parser = argparse.ArgumentParser(description='Python Converter for conversion of mixed type MOs to all-cartesian MOs for CHAMP code')
 
 # Required positional argument
-parser.add_argument("--lcao", dest='filename_lcao', type=str,
+parser.add_argument("--lcao", "-s", "--orb", dest='filename_lcao', type=str,
                     help='Required: Filename (including extension) of .lcao or .orb file in the old format')
 
 # Required positional argument
-parser.add_argument("--bfinfo", dest='filename_bfinfo', type=str,
+parser.add_argument("--bfinfo", "-r", "--bf", dest='filename_bfinfo', type=str,
                     help='Required: Filename (including extension) of .bfinfo file in the old format')
 
 # Required positional argument
-parser.add_argument("--geom", dest='filename_geom', type=str,
+parser.add_argument("--geom", "-g", "--xyz", dest='filename_geom', type=str,
                     help='Required: Filename of .geom file in the old format')
-
 
 args = parser.parse_args()
 
@@ -58,7 +57,6 @@ with open(args.filename_geom) as f2:
     dict_atom_type = {}
     atom_type = []
     for line_num, line in enumerate(lines):
-        print (line, line_num)
         # Skip the comments
         if line.startswith('&atoms'):
             nctype = int(line.split()[2])
@@ -86,6 +84,16 @@ with open(args.filename_geom) as f2:
 
     print (atom_type_symbol)
 
+## utility function to get the patterns in bfinfo file
+def guess_seq_len(seq):
+    guess = 1
+    max_len = int(len(seq) / 2)
+    for x in range(2, max_len):
+        print ("all ", seq[0:x])
+        if seq[0:x] == seq[x:2*x] :
+            print ("squence ", seq[0:x])
+            return x
+    return guess
 
 
 ### Read the bfinfo file
@@ -119,3 +127,54 @@ with open(args.filename_bfinfo) as f3:
         dict_radial_pointers[i] = lines[i].split()
 
     print ("radial pointers", dict_radial_pointers)
+
+
+
+### Writing part starts here!
+
+## Get the bfinfo file with cartesian ordering of shell pointers
+# Cartesian ordering
+order = [[0],
+         [0, 1, 2],
+         [0, 1, 2, 3, 4, 5],
+         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
+
+dict_l_of_shells = {1:0, 3:1, 6:2, 10:3}
+
+
+
+for vals in dict_radial_pointers.values():
+    bfcounter = 1; bf_representation = {}
+    counter0 = 0; basis_per_atom_counter = 0
+    pointers = [int(i) for i in vals]
+    sorted_pointers = sorted(pointers)
+    unique_shells, shell_count = np.unique(sorted_pointers, return_counts=True)
+    print ("unique inds   ", shell_count)
+
+    # Counter of how many p,d,f shells for the given atom
+    _, counter = np.unique(shell_count, return_counts=True)
+    print ("counter of shells per atom ", counter)
+    if len(counter) == 4:
+        local_ind_p = np.zeros((3,counter[1]),dtype=int)
+        local_ind_d = np.zeros((3,counter[2]),dtype=int)
+        local_ind_f = np.zeros((3,counter[3]),dtype=int)
+    elif len(counter) == 3:
+        local_ind_p = np.zeros((3,counter[1]),dtype=int)
+        local_ind_d = np.zeros((3,counter[2]),dtype=int)
+    elif len(counter) == 2:
+        local_ind_p = np.zeros((3,counter[1]),dtype=int)
+
+
+    for j in shell_count:
+        # local np array for reshuffling later
+        print ("the shell count", j)
+        l = dict_l_of_shells[j]
+        print ("the l", l)
+        print (counter0)
+        for k in order[l]:
+            bf_representation[counter0] = bfcounter
+            counter0 += 1
+            basis_per_atom_counter += 1
+        bfcounter += 1
+
+    print ("bf repre ", bf_representation)
